db3449353e6fd3ef18e4253885375e3f2ef0d42c
bde574786 <jungminsu07150@gmail.com>
2024-02-28 13:21:25 +0900

    속성 수정

--- static/js/edit.js
+++ static/js/edit.js
@@ -2,7 +2,7 @@ document.getElementById('post_button').addEventListener("click", function () {
 
     var post_id = document.getElementById('post_id').value;
     var titleValue = document.querySelector('.board_write .title input[type="text"]').value;
-    var writerValue = document.querySelector('.board_write .writer input[type="text"]').value;
+    var writerValue = document.querySelector('.board_write .info input[type="text"]').value;
     var contentValue = document.querySelector('.board_write .content textarea').value;
 
     post = {"id": post_id, "title": titleValue, "writer": writerValue, "content": contentValue}
